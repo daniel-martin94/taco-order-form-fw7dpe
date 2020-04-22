bfd5d418fafebe86133e9a8bc6d22599d4f312e7
daniel-martin94 <danielpmartin94@gmail.com>
2020-04-22 11:40:03 -0700

    1 file modified

--- components/OrderForm.js
+++ components/OrderForm.js
@@ -13,13 +13,14 @@ const OrderForm = ({ currentIngredient, changeCurrentIngredient, addToOrder, add
   const [checked, checkToggle] = useState(false)
 
   function optionDecide(type) {
-    if (type == 'shells' || type == 'base_layers') {
-      return currentIngredient.data.map(function (element, index) {
-        return (
-          <Form.Field key={element.id}>
-            <Grid>
-              <Grid.Column width={10}>
-                <Radio
+    return currentIngredient.data.map(function (element, index) {
+      return (
+        <Form.Field key={element.id}>
+          <Grid>
+            <Grid.Column width={10}>
+              { 
+                (type == 'shells' || type == 'base_layers') && 
+                  <Radio
                   label={element.name}
                   name='radioGroup'
                   value={element.id}
@@ -30,67 +31,49 @@ const OrderForm = ({ currentIngredient, changeCurrentIngredient, addToOrder, add
                     addSingleItem(element);
                   }}
                 />
-              </Grid.Column>
-              {element.price && element.price > 0 &&
-                <Grid.Column width={6}>
-                  <Label color="green" key={element.id}>
-                    <Icon name='dollar' /> {element.price}
-                  </Label>
-                </Grid.Column>
               }
-            </Grid>
-            <Divider />
-          </Form.Field>
-        )
-      })
-    }
-    //Other types of ingredients
-    return currentIngredient.data.map(function (element, index) {
-        return (
-          <Form.Field key={element.id}>
-            <Grid>
-              <Grid.Column width={10}>
-                <Checkbox 
-                label={element.name} 
-                value={element.id}
-                key={element.id}
-                onClick={() => {
-                  addMultipleItem(element);
-                }}
+              { 
+                (type != 'shells' && type != 'base_layers') &&
+                  <Checkbox
+                  label={element.name}
+                  value={element.id}
+                  key={element.id}
+                  onClick={() => {
+                    addMultipleItem(element);
+                  }}
                 />
-              </Grid.Column>
-              {element.price && element.price > 0 &&
-                <Grid.Column width={6}>
-                  <Label color="green">
-                    <Icon name='dollar' /> {element.price}
-                  </Label>
-                </Grid.Column>
               }
-            </Grid>
-            <Divider />
-          </Form.Field>
-        )
-      })
-    
+              </Grid.Column>
+            {element.price && element.price > 0 &&
+              <Grid.Column width={6}>
+                <Label color="green" key={element.id}>
+                  <Icon name='dollar' /> {element.price}
+                </Label>
+              </Grid.Column>
+            }
+          </Grid>
+          <Divider />
+        </Form.Field>
+      )
+    })
   }
-  //console.log(multipleSelection)
-  return (
-    <div>
-      <Segment>
-        <Header as="h3">{questions[currentIngredient['type']]}</Header>
-        <Form>
-          {optionDecide(currentIngredient['type'])}
-        </Form>
-        <Button
-          content="Next"
-          size="medium"
-          icon="arrow circle right"
-          onClick={changeCurrentIngredient}
-          labelPosition="right"
-        />
-      </Segment>
-    </div>
-  )
+return (
+  <div>
+    <Segment>
+      <Header as="h3">{questions[currentIngredient['type']]}</Header>
+      <Form>
+        {optionDecide(currentIngredient['type'])}
+      </Form>
+      <Button
+        content="Next"
+        size="medium"
+        icon="arrow circle right"
+        onClick={changeCurrentIngredient}
+        labelPosition="right"
+      />
+    </Segment>
+  </div>
+)
 }
 
 
